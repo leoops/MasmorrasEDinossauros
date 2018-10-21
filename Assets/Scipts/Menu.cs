69461e084435b83d7de5ed8212d557a8d4ec0b69
Leonardo Pereira <leonardopereira519@gmail.com>
2018-10-20 23:29:02 -0300

    Inclusao de melhorias
    - Adição basica de layout ao menu
    - inclusao de butao load game
    - melhora na transição de scenas
    - inclusoa de passagem de parametros

--- Assets/Scipts/Menu.cs
+++ Assets/Scipts/Menu.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
+    public Button loadMenu;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("LastLevelIndex") == 0)
+        {
+            loadMenu = GameObject.Find("LoadGame").GetComponent<Button>();
+            loadMenu.interactable = false;
+        }
+    }
 
     public void NewGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        int level = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+        PlayerPrefs.SetInt("LastLevelIndex", level);
+        PlayerPrefs.SetInt("PlayerCoins", 0);
     }
-
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("LastLevelIndex"), LoadSceneMode.Single);
+    }
     public void Quit()
     {
         #if UNITY_EDITOR
